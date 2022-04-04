0f4772682ed3dc9971140f0de847c7b085cbdcfd
Aurel1701 <gantzfan96@gmail.com>
2022-04-04 16:18:58 +0200

    edit

---
+++ assets/main.js
@@ -0,0 +1,13 @@
+const cells = document.querySelector('.cell')
+
+
+  function hard(params) {
+      for (let i = 0; i < 100; i++) {
+        document.querySelector("play").addEventListener("click",);
+        document.querySelector('.cells').insertAdjacentHTML
+          
+          
+      }
+      
+  }
+  hard()
