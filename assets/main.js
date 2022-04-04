b067fa66ea0ca8463e2c1cf2d96bd9222464042a
Aurel1701 <gantzfan96@gmail.com>
2022-04-04 17:22:57 +0200

    edit

--- assets/main.js
+++ assets/main.js
@@ -1,27 +1,39 @@
-document.querySelector(".play").addEventListener("click",print);
+document.querySelector(".play").addEventListener("click", print);
+document.querySelector('.cells').addEventListener('click', colorBlue)
+
+function print(params) {
+
+    let difficoltà = document.querySelector('#difficoltà').value
+    console.log(difficoltà);
+
+    if (difficoltà === 'easy') {
+        for (let i = 1; i <= 100; i++) {
+            document.querySelector('.container').insertAdjacentHTML('beforeend', ` <div class="cells">${i}  
+            </div>`)
+        }
+
+    } else if (difficoltà == 'medium') {
+        for (let i = 1; i <= 81; i++) {
+            document.querySelector('.container').insertAdjacentHTML('beforeend', ` <div class="cells">${i}  
+            </div>`)
+        }
+    } else if (difficoltà == 'hard') {
+        for (let i = 1; i <= 49; i++) {
+            document.querySelector('.container').insertAdjacentHTML('beforeend', ` <div class="cells">${i}  
+            </div>`)
+        }
 
 
-  function print(params) {
+    }
+    
 
-      let difficoltà = document.querySelector('#difficoltà').value
-      console.log(difficoltà);
 
-      if (difficoltà == easy) {
-          
-      }else if (difficoltà == medium){
+}
 
-      } else if (difficoltà == hard) {
+function colorBlue() {
 
-      }
+    document.querySelector('.cells').classList.add('blue')
 
-      
-      for (let i = 1; i <= 100; i++) {
-        document.querySelector('.container').insertAdjacentHTML('beforeend',` <div class="cells">${i}  
-        </div>`)
-          
-          
-      }
 
-      
-  }
-  
+
+}
